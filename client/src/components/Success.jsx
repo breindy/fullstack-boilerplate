0fbc86181d252692447465d18e25e01fb86c28b0
breindy <michelle.uy98@gmail.com>
2019-11-30 01:34:49 -0500

    login frontend validation, & backend db login validation

---
+++ client/src/components/Success.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Success extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<h2>You have successfully logged in!</h2>
+			</div>
+		);
+	}
+}
+
+export default Success;
